e877cd1126d6c0d949527cb19bb4b63dec5d4bbb
Wadson <wadson.dev92@gmail.com>
2021-03-24 18:53:13 -0300

    Exercicio 4.1

---
+++ sd-11/script.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = 2;
+let resultado;
+
+resultado = a + b;
+console.log("A soma de a + b é igual a: " + resultado);
+
+resultado = a - b;
+console.log("A subtração de a - b é igual a: " + resultado);
+
+resultado = a * b;
+console.log("A multiplicação de a * b é igual a: " + resultado);
+
+resultado = a / b;
+console.log("A divisão de a / b é igual a: " + resultado);
+
+resultado = a % b;
+console.log("O mod de a por b é igual a: " + resultado);
