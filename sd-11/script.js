85e5ff565ae8bfa8156142b0d33a03deb745a751
Wadson <wadson.dev92@gmail.com>
2021-03-25 15:27:46 -0300

    exercicios 4.1

--- sd-11/script.js
+++ sd-11/script.js
@@ -1,18 +1,44 @@
 let a = 5;
-let b = 2;
-let resultado;
+let b = 11;
+let c = 10;
+// let resultado;
 
-resultado = a + b;
-console.log("A soma de a + b é igual a: " + resultado);
+// Exercicio 01
+// resultado = a + b;
+// console.log("A soma de a + b é igual a: " + resultado);
 
-resultado = a - b;
-console.log("A subtração de a - b é igual a: " + resultado);
+// resultado = a - b;
+// console.log("A subtração de a - b é igual a: " + resultado);
 
-resultado = a * b;
-console.log("A multiplicação de a * b é igual a: " + resultado);
+// resultado = a * b;
+// console.log("A multiplicação de a * b é igual a: " + resultado);
 
-resultado = a / b;
-console.log("A divisão de a / b é igual a: " + resultado);
+// resultado = a / b;
+// console.log("A divisão de a / b é igual a: " + resultado);
 
-resultado = a % b;
-console.log("O mod de a por b é igual a: " + resultado);
+// resultado = a % b;
+// console.log("O mod de a por b é igual a: " + resultado);
+
+// Exercício 02
+// if (a > b) {
+//     console.log(a);
+// }
+// else {
+//     console.log(b);
+// }
+
+// Exercicio 03
+// if (a > b && b < c) {
+//     console.log(c);
+// }
+// else {
+//     console.log("Não retorna nada");
+// }
+
+// Exercicio 05
+// if (a+b+c >= 3 && a+b+c <= 180){
+// console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
